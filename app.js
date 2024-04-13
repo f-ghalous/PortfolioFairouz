a3fefcfc5689df9f797f26960d4ef1a80825d033
f-ghalous <e2295817@cmaisonneuve.qc.ca>
2024-04-13 18:37:11 -0400

    semble une erreur dans le html

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-const  btnMenu = document.querySelector('.btn-rond-menu');
+const   btnMenu = document.querySelector('.btn-rond-menu');
 const nav = document.querySelector('.nav-gauche');
 const allItemNav = document.querySelectorAll('.nav-menu-item');
 const ligne = document.querySelector('.cont-ligne');
+ btnMenu.addEventListener('click', ()=>{
+    ligne.classList.toggle('active');
+ })
