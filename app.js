5ad17d0ef6e962d011c8c8c41383439a41e03b18
f-ghalous <e2295817@cmaisonneuve.qc.ca>
2024-04-13 18:24:34 -0400

    animer le bouton

---
+++ app.js
@@ -0,0 +1,4 @@
+const  btnMenu = document.querySelector('.btn-rond-menu');
+const nav = document.querySelector('.nav-gauche');
+const allItemNav = document.querySelectorAll('.nav-menu-item');
+const ligne = document.querySelector('.cont-ligne');
